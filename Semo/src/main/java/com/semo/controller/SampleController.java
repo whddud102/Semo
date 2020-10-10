a102c7e8d6be8d8383f1559498a7add173cab54f
JongYoung <whddud102@naver.com>
2020-10-10 19:42:30 +0900

    log 객체 추가

--- Semo/src/main/java/com/semo/controller/SampleController.java
+++ Semo/src/main/java/com/semo/controller/SampleController.java
@@ -3,11 +3,15 @@ package com.semo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.java.Log;
+
+@Log
 @RestController
 public class SampleController {
 	
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("로그 추가");
 		return "hi, 나는 정상 작동 중 7번째";
 	}
 }
