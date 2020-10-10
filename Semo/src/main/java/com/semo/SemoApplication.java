a767cf0ab9a1cc72e7ebc966ddc5692d10f313c7
JongYoung <whddud102@naver.com>
2020-10-10 15:55:34 +0900

    inital commit

---
+++ Semo/src/main/java/com/semo/SemoApplication.java
@@ -0,0 +1,13 @@
+package com.semo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemoApplication.class, args);
+	}
+
+}
